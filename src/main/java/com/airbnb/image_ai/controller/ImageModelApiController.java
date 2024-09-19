c1afeb88d0d1a879803d1d8fc604b476b9b5173d
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-09-20 00:00:53 +0900

    feat: 이미지 인풋 데이터를 전체 받아서 완전한 이미지로 분석하도록 변경 -> 향후 컨트롤러 리팩토링 필요함

--- src/main/java/com/airbnb/image_ai/controller/ImageModelApiController.java
+++ src/main/java/com/airbnb/image_ai/controller/ImageModelApiController.java
@@ -3,12 +3,15 @@ package com.airbnb.image_ai.controller;
 import com.airbnb.image_ai.service.ImageModelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.io.InputStream;
 
 /**
  * packageName    : com.airbnb.image_ai.controller
@@ -31,9 +34,16 @@ public class ImageModelApiController {
     @PostMapping(value = "/classify")
     public Mono<String> classifyImage(@RequestPart("imageFile") Mono<FilePart> filePartMono) {
         return filePartMono
-                .flatMap(filePart -> filePart.content()
-                        .map(dataBuffer -> dataBuffer.asInputStream(true))
-                        .next())
-                .flatMap(imageModelService::classifyImage);
+                .flatMap(filePart -> DataBufferUtils.join(filePart.content())
+                        .map(dataBuffer -> {
+                            InputStream is = dataBuffer.asInputStream(true);
+                            DataBufferUtils.release(dataBuffer); // dataBuffer 메모리 누수 방지
+                            return is;
+                        }))
+                .flatMap(imageModelService::classifyImage)
+                .onErrorResume(e -> {
+                    log.error("Image classification failed", e);
+                    return Mono.just("Image classification failed");
+                });
     }
 }
